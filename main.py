2dacb4e83376eef4d666918848dd8092dd401fa2
NeilNeel <neelbarvaliya04@gmail.com>
2025-03-17 23:12:41 -0400

    refactor get_book_text to return a formatted report of word and character counts

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from stats import get_word_count, get_character_count
+from stats import get_word_count, get_character_count, report
 
 def get_book_text(book_path):
     with open(book_path, 'r', encoding="utf-8") as f:
         file_contents = f.read()
         word_count = get_word_count(file_contents)
         char_count = get_character_count(file_contents)
-        print((f"{word_count} words found in the document"))
-        print(char_count)
+        return report(word_count, char_count)
+
 
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
-
+    get_book_text("./books/frankenstein.txt")
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,20 @@ def get_character_count(text_content):
             else:
                 char_count[char] = 1
     return char_count
+
+# def sort_on(dict):
+#     return dict
+
+def report(word_count, char_count):
+    sorted_char_count = dict(sorted(char_count.items(), key=lambda item: item[1], reverse=True))
+    report_output = "============ BOOKBOT ============\n"
+    report_output += "Analyzing book found at books/frankenstein.txt...\n"
+    report_output += "----------- Word Count ----------\n"
+    report_output += f"Found {word_count} total words\n"
+    report_output += "--------- Character Count -------\n"
+
+    for key in sorted_char_count:
+        report_output += f"{key}: {char_count[key]}\n"
+
+    report_output += "============= END ==============="
+    return print(report_output)
