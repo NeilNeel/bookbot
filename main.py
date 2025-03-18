82147d7b79b22df132ff9bb9497f82228f8f2f08
NeilNeel <neelbarvaliya04@gmail.com>
2025-03-17 22:14:50 -0400

    update get_book_text to return word count instead of file content

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 def get_book_text(book_path):
     with open(book_path, 'r', encoding="utf-8") as f:
         file_contents = f.read()
-        return file_contents
+        return get_word_count(file_contents)
+
+def get_word_count(text_content):
+    words = text_content.split()
+    count = len(words)
+    return (f"{count} words found in the document")
 
 def main():
     print(get_book_text("./books/frankenstein.txt"))
+
 
 main()
